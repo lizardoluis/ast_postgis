48b778ff71d83fc6e81bdbc15d3121945df61d22
Luis Eduardo Oliveira Lizardo <lizardo.luis@gmail.com>
2016-09-25 16:06:34 -0300

    Implement function isTopologicalRelationship.
    This function checks in a try/catch manner if the topological relationship
    is valid.

--- sql/functions/utils.sql
+++ sql/functions/utils.sql
@@ -311,3 +311,19 @@ BEGIN
    RETURN r;
 END;
 $$  LANGUAGE plpgsql;
+
+
+--
+-- Check if the operator is a valid topological relationship
+--
+CREATE FUNCTION _omtg_isTopologicalRelationship(operator text) RETURNS BOOLEAN AS $$
+DECLARE
+   tr _omtg_topologicalrelationship;
+BEGIN
+   tr := operator;
+   RETURN TRUE;
+EXCEPTION
+   WHEN invalid_text_representation THEN
+      RETURN FALSE;
+END;
+$$  LANGUAGE plpgsql;
