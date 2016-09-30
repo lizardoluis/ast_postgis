98b2c51cc6fc39eec473953a03326123b058bf7b
Luis Eduardo Oliveira Lizardo <lizardo.luis@gmail.com>
2016-09-30 17:29:36 -0300

    Implement table omtg_violation_log.

--- sql/definitions/tables.sql
+++ sql/definitions/tables.sql
@@ -1,13 +1,14 @@
 --
 -- Spatial error log table
 --
---CREATE TABLE omtg_violation (
---    time timestamp,
---    type VARCHAR(50),
---    description TEXT
---);
+CREATE TABLE omtg_violation_log (
+   time timestamp,
+   type VARCHAR(50),
+   description VARCHAR(150)
+);
 
 --
--- Mark the omtg_violation table as a configuration table, which will cause pg_dump to include the table's contents (not its definition) in dumps.
+-- Mark the omtg_violation table as a configuration table, which will cause
+-- pg_dump to include the table's contents (not its definition) in dumps.
 --
---SELECT pg_catalog.pg_extension_config_dump('omtg_violation', '');
+SELECT pg_catalog.pg_extension_config_dump('omtg_violation_log', '');
