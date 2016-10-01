7dd8a50d9702edef93f45f6ceab59f6e4e6a1c44
Luis Eduardo Oliveira Lizardo <lizardo.luis@gmail.com>
2016-10-01 20:00:36 -0300

    Implement omtg_isTopologicalRelationshipValid function.

--- sql/functions/consistency_check.sql
+++ sql/functions/consistency_check.sql
@@ -2,7 +2,7 @@
 -- This function checks if all the elements of b_tbl is within the buffer
 -- distance from the elements of a_tbl.
 --
-create or replace function omtg_isNearValid(a_tbl text, a_geom text, b_tbl text, b_geom text, dist real)
+create function omtg_isNearValid(a_tbl text, a_geom text, b_tbl text, b_geom text, dist real)
    returns boolean as $$
 declare
    pkColumn text := _omtg_getPrimaryKeyColumn(b_tbl);
@@ -13,17 +13,47 @@ begin
          select now(),
                ''Near buffer violation'',
                ''Table ´'|| b_tbl ||'´ tuple with primary key ´''|| b.'|| pkColumn ||' ||''´ is outside the buffer distance of ´'|| dist ||'´ from table ´'|| a_tbl ||'´.''
-         from tableb b
-         where b.id not in
+            from '|| b_tbl ||' b
+            where b.'|| pkColumn ||' not in
+            (
+               select distinct b.'|| pkColumn ||'
+               from '|| a_tbl ||' a, '|| b_tbl ||' b
+               where st_dwithin(a.'|| a_geom ||', b.'|| b_geom ||', '|| dist ||')
+            )
+         ) RETURNING true;' into res;
+
+         if res then return false;
+         else return true;
+         end if;
+end;
+$$  language plpgsql;
+
+
+--
+-- This function checks if the topolotical relationship is violated.
+--
+create function omtg_isTopologicalRelationshipValid(a_tbl text, a_geom text, b_tbl text, b_geom text, relation _omtg_topologicalrelationship)
+   returns boolean as $$
+declare
+   pkColumn text := _omtg_getPrimaryKeyColumn(a_tbl);
+   res boolean;
+begin
+
+   EXECUTE 'INSERT into omtg_violation_log (time, type, description) (
+         select now(),
+               ''Topological relationship violation'',
+               ''Topological relationship ('|| relation ||') between ´'|| a_tbl ||'´ and ´'|| b_tbl ||'´ is violated by the tuple of ´'|| a_tbl ||'´ with primary key ´''|| a.'|| pkColumn ||' ||''´.''
+         from '|| a_tbl ||' a
+         where a.'|| pkColumn ||' not in
          (
-            select b.id
-            from tablea a, tableb b
-            where st_dwithin(a.geom, b.geom, '|| dist ||')
+            select distinct a.'|| pkColumn ||'
+            from '|| a_tbl ||' a, '|| b_tbl ||' b
+            where st_'|| relation ||'(a.'|| a_geom ||', b.'|| b_geom ||')
          )
       ) RETURNING true;' into res;
 
-      if res then return true;
-      else return false;
+      if res then return false;
+      else return true;
       end if;
 end;
 $$  language plpgsql;
