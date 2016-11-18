3eb19dee519332c51689f55719e2bc292df060ae
Luis Eduardo Oliveira Lizardo <lizardo.luis@gmail.com>
2016-11-17 23:10:17 -0200

    Fix bug on geometry type.

--- examples/transportation_system/ddl.sql
+++ examples/transportation_system/ddl.sql
@@ -2,7 +2,7 @@
 -- TRANSPORTATION SYSTEM
 --
 
--- Table creation
+-- Tables creation
 create table bus_line (
    line_number integer primary key,
    description varchar(50),
@@ -12,20 +12,20 @@ create table bus_line (
 create table school_district (
    district_name varchar(50) primary key,
    school_capacity integer,
-   geom omtg_polygon
+   geom ast_polygon
 );
 
 create table bus_stop (
    stop_id integer primary key,
    shelter_type varchar(50),
-   geom omtg_point
+   geom ast_node
 );
 
 create table bus_route_segment (
    traverse_time real,
    segment_number integer,
    busline integer references bus_line (line_number),
-   geom omtg_uniline
+   geom ast_uniline
 );
 
 --
@@ -34,7 +34,7 @@ create table bus_route_segment (
 CREATE TRIGGER school_district_contains_trigger
    AFTER INSERT OR UPDATE ON school_district
    FOR EACH STATEMENT
-   EXECUTE PROCEDURE omtg_topologicalrelationship('school_district', 'geom', 'bus_stop', 'geom', 'contains');
+   EXECUTE PROCEDURE ast_topologicalrelationship('school_district', 'geom', 'bus_stop', 'geom', 'contains');
 
 --
 -- Bus_route_segment and Bus_stop arc-node network constraints
@@ -42,4 +42,4 @@ CREATE TRIGGER school_district_contains_trigger
 CREATE TRIGGER busroute_insert_update_trigger
    AFTER INSERT OR UPDATE ON bus_route_segment
 	FOR EACH STATEMENT
-	EXECUTE PROCEDURE omtg_arcnodenetwork('bus_route_segment', 'geom', 'bus_stop', 'geom');
+	EXECUTE PROCEDURE ast_arcnodenetwork('bus_route_segment', 'geom', 'bus_stop', 'geom');
