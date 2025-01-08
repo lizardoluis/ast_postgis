a7b71d3b350cd7c12dfb574ea886bd14c870ce6c
Luís Lizardo <luis.lizardo@enterprisedb.com>
2025-01-08 11:35:04 -0300

    Add installation script for PostgreSQL 17

---
+++ install-ast_postgis-17.bat
@@ -0,0 +1,23 @@
+@echo off
+set /p PGUSER="Insert the username for PostgreSQL: "  
+set /p PGPASSWORD="Insert the password for PostgreSQL: "     
+set /p PGDATABASE="Insert the schema name to each ast_postgis should be installed: "
+
+        
+echo "Enabling the extension PostGIS"
+
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\postgis.sql"
+
+echo "Installing the ast_postgis."
+
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\tables.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\types.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\type_functions.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\utils.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\domain_functions.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\domains.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\relationship_triggers.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\event_triggers.sql"
+"C:/Program Files/PostgreSQL/17/bin/psql.exe" -h localhost < "%~dp0\sql\consistency_functions.sql"
+
+pause
